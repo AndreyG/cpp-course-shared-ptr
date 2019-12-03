3d32b8a32032f62a82fb1a4cea565f15e90aac0f
Andrey Davydov <andrey.a.davydov@gmail.com>
2019-12-03 18:56:15 +0300

    + support make_shared

--- shared_ptr.cpp
+++ shared_ptr.cpp
@@ -1,4 +1,5 @@
 ﻿#include <atomic>
+#include <new>
 
 struct control_block {
     virtual ~control_block() = default;
@@ -16,6 +17,28 @@ struct control_block_impl final : control_block {
         deleter(ptr);
     }
 };
+
+template<typename T>
+struct control_block_inplace final : control_block {
+    std::aligned_storage_t<sizeof(T), alignof(T)> storage;
+
+    T* get() { return reinterpret_cast<T*>(&storage); }
+
+    template<typename... Args>
+    control_block_inplace(Args &&... args) {
+        new(&storage) T(std::forward<Args>(args)...);
+    }
+
+    void destroy() override {
+        get()->~T();
+    }
+};
+
+template<typename T>
+class shared_ptr;
+
+template<typename T, typename... Args>
+shared_ptr<T> make_shared(Args &&...);
 
 template<typename T>
 class shared_ptr {
@@ -43,4 +66,18 @@ public:
             delete cb;
         }
     }
+
+private:
+    template<typename U, typename... Args>
+    friend shared_ptr<U> make_shared(Args &&...);
+
+    shared_ptr(control_block_inplace<T>* cb)
+        : ptr(cb->get())
+        , cb(cb)
+    {};
 };
+
+template<typename T, typename... Args>
+shared_ptr<T> make_shared(Args &&... args) {
+    return shared_ptr<T>(new control_block_inplace<T>(std::forward<Args>(args)...));
+}
